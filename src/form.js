17b4fccb18b1d00fee2a11699c4d8dab2f5ba3b0
Gali Kechichian <galialik.k@gmail.com>
2023-07-16 23:46:53 +0300

    contact form draft 3

--- src/form.js
+++ src/form.js
@@ -1,30 +1,64 @@
 /*Author: Gali Kechichian*/
 
-function validateName(nameInput){
-    return (nameInput !== "");
+const form = document.getElementById('myForm');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const msgInput = document.getElementById('message');
+
+function validateName(){
+    if (nameInput.value === "") {
+        nameInput.classList.add('invalid');
+        document.getElementById('name-error').classList.add('invalid');
+    }
+    else {
+        nameInput.classList.remove('invalid');
+        document.getElementById('name-error').classList.remove('invalid');
+    }
 }
 
-function validateEmail(emailInput) {
+function validateEmail() {
     let pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    return (emailInput.test(pattern) || emailInput !== "");
+    if (!pattern.test(emailInput.value) || emailInput.value === "") {
+        emailInput.classList.add('invalid');
+        document.getElementById('email-error').classList.add('invalid');
+    }
+    else {
+        emailInput.classList.remove('invalid');
+        document.getElementById('email-error').classList.remove('invalid');
+    }
 }
 
-function changeDisplays() {
-    // listen to the form submission event
-    let form = document.getElementById('myForm');
-    form.addEventListener('submit', (event)=>{
-        event.preventDefault();
-        let nameInput = document.getElementById('name');
-        let emailInput = document.getElementById('email');
-        if (!validateName(nameInput.value)) {
-            nameInput.classList.add('invalid');
-            document.getElementById('name-error').classList.add('invalid');
-        }
-        if (!validateName(emailInput.value)) {
-            emailInput.classList.add('invalid');
-            document.getElementById('email-error').classList.add('invalid');
-        }
-    });
+function validateMessage() {
+    if (msgInput.value === "") msgInput.classList.add('invalid');
+    else msgInput.classList.remove('invalid');
 }
-changeDisplays();
+
+
+form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+
+    validateName();
+    validateEmail();
+    validateMessage();
+
+    // call functions on key up:
+    nameInput.addEventListener('keyup', (event)=> {
+        validateName();
+        if (event.key === "Enter")
+        emailInput.focus();
+    });
+    emailInput.addEventListener('keyup', (event)=> {
+        validateEmail();
+        if (event.key === "Enter") msgInput.focus();
+    });
+    msgInput.addEventListener('keyup', validateMessage);
+
+    // if all of the inputs are valid, submit
+    if (!nameInput.classList.contains('invalid')
+    && !emailInput.classList.contains('invalid')
+    && !msgInput.classList.contains('invalid')) {
+        form.submit();
+    }
+
+})
 
